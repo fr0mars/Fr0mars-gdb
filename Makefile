a3327e918435d8d299075e38525bf416da7d6cd2
fr0mars <matteo.ahouanto@gmail.com>
2024-11-12 23:46:41 +0100

    array of breakpoints

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS=-Wall -Wextra -Wvla -g -std=c99 -pedantic
 all: dbg
 
 dbg: dbg.c
-	$(CC) $(CFLAGS) -o my-dbg dbg.c
+	$(CC) $(CFLAGS) -o my-dbg dbg.c utils.c
 
 clean:
-	rm -f dbg *.o
+	rm -f my-dbg *.o
--- dbg.c
+++ dbg.c
@@ -14,86 +14,15 @@
 
 */
 
-#include "dbg.h"
-
-static struct bp set_breakpoint(pid_t pid, size_t address)
-{
-    struct bp res;
-    res.address = address;
-    res.data = ptrace(PTRACE_PEEKDATA, pid, address, 0);
-
-    size_t data = 0xCC;
-    ptrace(PTRACE_POKEDATA, pid, address, data);
-    return res;
-}
-
-static void remove_breakpoint(pid_t pid, struct bp bp)
-{
-    ptrace(PTRACE_POKEDATA, pid, bp.address, bp.data);
-}
-
-static void rm_n(char cmd[])
-{
-    for (int i = 0; cmd[i] != 0; i++)
-    {
-        if (cmd[i] == '\n')
-            cmd[i] = 0;
-    }
-}
-
-static void print_regs(struct user_regs_struct registers)
-{
-    printf("rip: 0x%llx\n", registers.rip);
-    printf("rsp: 0x%llx\n", registers.rsp);
-    printf("rbp: 0x%llx\n", registers.rbp);
-    printf("eflags: 0x%llx\n", registers.eflags);
-    printf("orig_rax: 0x%llx\n", registers.orig_rax);
-    printf("rax: 0x%llx\n", registers.rax);
-    printf("rbx: 0x%llx\n", registers.rbx);
-    printf("rcx: %llx\n", registers.rcx);
-    printf("rdx: 0x%llx\n", registers.rdx);
-    printf("rdi: 0x%llx\n", registers.rdi);
-    printf("rsi: 0x%llx\n", registers.rsi);
-    printf("r8: 0x%llx\n", registers.r8);
-    printf("r9: 0x%llx\n", registers.r9);
-    printf("r10: 0x%llx\n", registers.r10);
-    printf("r11: 0x%llx\n", registers.r11);
-    printf("r12: 0x%llx\n", registers.r12);
-    printf("r13: 0x%llx\n", registers.r13);
-    printf("r14: 0x%llx\n", registers.r14);
-    printf("r15: 0x%llx\n", registers.r15);
-    printf("cs: 0x%llx\n", registers.cs);
-    printf("ds: 0x%llx\n", registers.ds);
-    printf("es: 0x%llx\n", registers.es);
-    printf("fs: 0x%llx\n", registers.fs);
-    printf("gs: 0x%llx\n", registers.gs);
-    printf("ss: 0x%llx\n", registers.ss);
-    printf("fs_base: 0x%llx\n", registers.fs_base);
-    printf("gs_base: 0x%llx\n\n", registers.gs_base);
-}
-
-static void print_maps(pid_t pid)
-{
-    char maps[256];
-    sprintf(maps, "/proc/%d/maps", pid);
-    FILE *fp = fopen(maps, "r");
-    if (fp == NULL)
-    {
-        printf("Error\n");
-        exit(1);
-    }
-    char line[2048];
-    while (fgets(line, sizeof(line), fp) != NULL)
-    {
-        printf("%s", line);
-    }
-    fclose(fp);
-    printf("\n");
-}
+#include "utils.h"
 
 static void debug(pid_t pid)
 {
-    char cmd[128]; // arbitrary size atm
+    char cmd[256];
+    struct bp *arr_bp = malloc(sizeof(struct bp) * 128);
+    int nb_bp = 0;
+
+    // int *status;
     while (1)
     {
         printf("dbg> ");
@@ -106,7 +35,8 @@ static void debug(pid_t pid)
             printf("info_regs: display registers\n\
 continue: continue program\n\
 quit: exit\n\
-help: display this\n");
+help: display this\n\
+info_memory: prints mem maps\n");
         }
 
         else if (strcmp(ptr, "quit") == 0)
@@ -128,16 +58,13 @@ help: display this\n");
         {
             char *addr = strtok(NULL, " ");
             size_t address = strtol(addr, NULL, 16);
-            struct bp bp = set_breakpoint(
-                pid,
-                address); // have to do a linked list of breakpoints to log them
-            // printf("breakpoint set at 0x%lx\n", address);
+            set_breakpoint(pid, address, arr_bp, &nb_bp);
         }
 
         else if (strcmp(ptr, "continue") == 0)
         {
             ptrace(PTRACE_CONT, pid, 0, 0);
-            continue;
+            waitpid(pid, NULL, 0);
         }
 
         else if (strcmp(ptr, "step") == 0)
